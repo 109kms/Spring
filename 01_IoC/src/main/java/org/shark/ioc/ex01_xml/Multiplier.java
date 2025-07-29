ef67c349cd49f263b72def604b55e947fcd8e904
109kms <109kms@naver.com>
2025-07-29 15:32:02 +0900

    250729

---
+++ 01_IoC/src/main/java/org/shark/ioc/ex01_xml/Multiplier.java
@@ -0,0 +1,16 @@
+package org.shark.ioc.ex01_xml;
+
+public class Multiplier {
+
+  public int multiply(int... args) {
+    int total = 1;
+    if (args == null) {
+      return 0;
+    }
+    for (int i = 0; i <= args.length; i++) {
+      total *= args[i];
+    }
+    return total;
+  }
+
+}
